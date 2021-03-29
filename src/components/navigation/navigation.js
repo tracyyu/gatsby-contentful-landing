422d0304dba2221601a17d353a5bccf08204fcdd
Tracy Yu <tracy@sunset.health>
2021-03-28 23:40:37 -0700

    Add responsive styling for mobile

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
 import Button from '../button/button'
+import hamburger from '../../../static/hamburger.svg';
 import * as styles from './navigation.module.css'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const Navigation = (props) => {
+
+  const [open, setOpen] = useState(false);
 
   const {contentfulHeader : { companyLogo, headerNavBar }} = useStaticQuery(graphql`
   query {
@@ -32,13 +35,43 @@ const Navigation = (props) => {
   }
   `)
 
-  console.log(companyLogo.file.url)
+  const openHamburgerNav = () => {
+    setOpen(!open);
+  }
 
   return (
     <div className={styles.navigationContainer}>
       <nav role="navigation" className={styles.navigationWrapper}>
         <img className={styles.privacy} alt="logo" src={`https:${companyLogo.file.url}`} />
         <ul className={styles.navigation}>
+          {
+            headerNavBar.navbarItems.map((item) => {
+              return (
+                <li className={styles.navigationItem} key={item.id}>
+                  <Link to="/">{item.label}</Link>
+                </li>
+              )
+            })
+          }
+          {
+            headerNavBar.ctaButtons.map((button) => {
+              return (
+                <li className={styles.navigationItem} key={button.name}>
+                  <Button 
+                    type={button.type}
+                    text={button.text}
+                  />
+                </li>
+              )
+            })
+          }
+        </ul>
+      </nav>
+      <nav role="navigation" className={styles.hamburgerNav}>
+        <img className={styles.privacy} alt="logo" src={`https:${companyLogo.file.url}`} />
+        <hr />
+        <img src={hamburger} className={styles.buttonIcon} onClick={() => openHamburgerNav()} />
+        <ul className={styles.navigation} style={{ display: open ? 'flex' : 'none' }}>
           {
             headerNavBar.navbarItems.map((item) => {
               return (
